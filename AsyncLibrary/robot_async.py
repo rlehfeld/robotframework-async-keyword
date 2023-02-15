c2434a4c6e04c13bed79d67bdf531c4cb3ab1cff
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2023-02-15 19:05:06 +0100

    make library work again with newer robot framework versions, implement some error check and handle exceptions

--- AsyncLibrary/robot_async.py
+++ AsyncLibrary/robot_async.py
@@ -1,52 +1,43 @@
-import sys
-import os
-import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
 from robot.libraries.BuiltIn import BuiltIn
-from robot.output import librarylogger as log
 from robot.running import Keyword
-from robot.running.context import EXECUTION_CONTEXTS
+from robot.running.userkeyword import UserKeywordRunner
+
 
 class AsyncLibrary:
     def __init__(self):
-        self._thread_pool = {}
+        self._future = {}
         self._last_thread_handle = 0
+        self._executor = ThreadPoolExecutor()
+        self._lock = threading.Lock()
 
-    def async_run(self, keyword, *args, **kwargs):
-        ''' Executes the provided Robot Framework keyword in a separate thread and immediately returns a handle to be used with async_get '''
-        handle = self._last_thread_handle
-        thread = self._threaded(keyword, *args, **kwargs)
-        thread.start()
-        self._thread_pool[handle] = thread
-        self._last_thread_handle += 1
+    def async_run(self, keyword, *args):
+        '''
+        Executes the provided Robot Framework keyword in a separate thread
+        and immediately returns a handle to be used with async_get
+        '''
+        context = BuiltIn()._get_context()
+        runner = context.get_runner(keyword)
+        if isinstance(runner, UserKeywordRunner):
+            raise ValueError(
+                'async_run cannot be used for user defined '
+                'keywords as the output xml file will get corrupted'
+            )
+        with self._lock:
+            handle = self._last_thread_handle
+            self._last_thread_handle += 1
+        self._future[handle] = self._executor.submit(
+            runner.run, Keyword(keyword, args=args), context
+        )
         return handle
 
-    def async_get(self, handle):
-        ''' Blocks until the thread created by async_run returns '''
-        assert handle in self._thread_pool, 'Invalid async call handle'
-        result = self._thread_pool[handle].result_queue.get()
-        del self._thread_pool[handle]
-        return result
-
-    def _get_handler_from_keyword(self, keyword):
-        ''' Gets the Robot Framework handler associated with the given keyword '''
-        if EXECUTION_CONTEXTS.current is None:
-            raise RobotNotRunningError('Cannot access execution context')
-        return EXECUTION_CONTEXTS.current.get_handler(keyword)
-
-    def _threaded(self, keyword, *args, **kwargs):
+    def async_get(self, handle, timeout=None):
+        '''
+        Blocks until the future created by async_run includes a result
+        '''
         try:
-            import queue
-        except ImportError:
-            import Queue as queue
-        import threading
-        
-        def wrapped_f(q, *args, **kwargs):
-            ''' Calls the decorated function and puts the result in a queue '''
-            f = self._get_handler_from_keyword(keyword)
-            ret = f.run(EXECUTION_CONTEXTS.current, args)
-            q.put(ret)
-
-        q = queue.Queue()
-        t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
-        t.result_queue = q
-        return t
+            future = self._future.pop(handle)
+        except KeyError:
+            raise ValueError('entry with handle {handle} does not exist')
+        return future.result(timeout)
