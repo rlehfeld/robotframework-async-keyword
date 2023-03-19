86e9edb01299a34a248bb281098d0609064b6186
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2023-03-19 10:24:19 +0100

    use new version for latest changes which are compatible with windows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='robotframework-async',
-    version='1.1.1',
+    version='1.1.2',
     description=(
         'Generic Robot Framework library for asynchronous keyword execution'
     ),
