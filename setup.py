7985b07f0f112b2673502333fe71d424529d1aad
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2023-03-20 16:07:34 +0100

    use more modern packaging

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-'''
-setup/install instructions for pip
-'''
 from setuptools import setup
 
 setup(
-    name='robotframework-async-keyword',
-    version='1.1.4',
-    description=(
-        'Generic Robot Framework library for asynchronous keyword execution'
-    ),
-    author='René Lehfeld',
-    author_email='54720674+rlehfeld@users.noreply.github.com',
-    license='MIT',
-    url='https://github.com/rlehfeld/robotframework-async-keyword',
-    download_url='https://github.com/rlehfeld/robotframework-async-keyword',
-    keywords=['async', 'robotframework'],
-    install_requires=['robotframework >= 5.0.1'],
-    packages=['AsyncLibrary'],
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Development Status :: 4 - Beta',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
